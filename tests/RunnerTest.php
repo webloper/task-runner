f95e6531126e530d9e3f73e4a517aeefb711a64f
OneDios <onedios2019@gmail.com>
2022-02-13 02:47:35 +0530

    Initial tests

--- tests/RunnerTest.php
+++ tests/RunnerTest.php
@@ -5,8 +5,61 @@ declare(strict_types=1);
 namespace Webloper\TaskRunner\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Webloper\TaskRunner\Runner;
+use Webloper\TaskRunner\Tasks\NullTask;
 
 class RunnerTest extends TestCase
 {
-    
+    protected function runner(array $tasks = []): Runner
+    {
+        return Runner::prepare($tasks);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_build_a_runner()
+    {
+        $this->assertInstanceOf(
+            Runner::class,
+            $this->runner(),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_starts_a_runner_with_empty_array()
+    {
+        $runner = $this->runner();
+
+        $this->assertIsArray(
+            $runner->tasks()
+        );
+
+        $this->assertEmpty(
+            $runner->tasks()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_add_a_new_task()
+    {
+        $runner = $this->runner();
+
+        $task = $this->getMockForAbstractClass(NullTask::class);
+
+        $runner->add($task);
+
+        $this->assertNotEmpty(
+            $runner->tasks()
+        );
+
+        $this->assertCount(
+            1,
+            $runner->tasks()
+        );
+    }
 }
